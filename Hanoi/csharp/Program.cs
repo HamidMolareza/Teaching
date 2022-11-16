ccd33f54f9873d33e7af9147ba69a1c19c409cff
Hamid Molareza <kitm.hamid@gmail.com>
2022-11-16 21:59:38 +0330

    Adds csharp  solution to hanoi

---
+++ Hanoi/csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Number of disks: ");
+var n = Convert.ToInt32(Console.ReadLine());
+Hanoi("1", "2", "3", n);
+
+void Hanoi(string fromBar, string toBar, string helpBar, int n) {
+    if (n == 1) {
+        PrintMove(fromBar, toBar);
+    }
+    else {
+        Hanoi(fromBar, helpBar, toBar, n - 1);
+        PrintMove(fromBar, toBar);
+        Hanoi(helpBar, toBar, fromBar, n - 1);
+    }
+}
+
+void PrintMove(string fromBar, string toBar) {
+    Console.WriteLine($"Move from {fromBar} to {toBar}");
+}
