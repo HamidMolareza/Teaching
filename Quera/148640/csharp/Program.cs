d037d5084abe61f86cf38615af327082bed30bfc
Hamid Molareza <kitm.hamid@gmail.com>
2022-11-20 13:30:51 +0330

    Solves 148640 with csharp

--- Quera/148640/csharp/Program.cs
+++ Quera/148640/csharp/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace csharp;
 
-Console.WriteLine("Hello, World!");
+public static class Program {
+    public static void Main() {
+        var numOfQuestions = Convert.ToInt32(Console.ReadLine());
+        var keys = Console.ReadLine();
+        var numOfPersons = Convert.ToInt32(Console.ReadLine());
+
+        for (var i = 0; i < numOfPersons; i++) {
+            var personAnswers = new List<string>(numOfQuestions);
+            for (var j = 0; j < numOfQuestions; j++)
+                personAnswers.Add(Console.ReadLine());
+
+            var score = CalculateScore(personAnswers, keys);
+            Console.WriteLine(score);
+        }
+    }
+
+    private static int CalculateScore(this IReadOnlyList<string> personAnswers, string keys) {
+        var correctAnswers = 0;
+        var wrongAnswers = 0;
+
+        for (var i = 0; i < keys.Length; i++) {
+            var markedCount = personAnswers[i].Count(a => a == '#');
+            if (markedCount < 1)
+                continue;
+
+            if (markedCount > 1 || personAnswers[i][keys[i] - 'A'] != '#')
+                wrongAnswers++;
+            else
+                correctAnswers++;
+        }
+
+        return (3 * correctAnswers) - wrongAnswers;
+    }
+}
